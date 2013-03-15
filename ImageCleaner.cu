cfb8395291a7db579a3b6cd547a9de17dfc2026d
James Whitbeck <james.b.whitbeck@gmail.com>
2013-03-15 13:24:15 -0700

    test

--- ImageCleaner.cu
+++ ImageCleaner.cu
@@ -586,7 +586,7 @@ __global__ void forwardFFTRow(float *real_image, float *imag_image)
   //remove
   log_size = 1;
   while ((1 << log_size) < SIZE)
-    log_size <<=1;
+    log_size +=1;
   char curr = forwardFFT_any(real, imag, 0, 1, log_size, 0);
 
   real_image[offset] = real[curr][col];
